924c41d18a4967c353dc98fe98f50f49b56d3ddd
kateryna kuzenkova <kompanenko96@gmail.com>
2019-12-24 22:20:04 +0200

    Added task 2

--- src/com/epam/lessons/lesson1/Task1.java
+++ src/com/epam/lessons/lesson1/Task1.java
@@ -1,4 +1,4 @@
-package com.epam.lesson1.Lesson1;
+package com.epam.lessons.lesson1;
 
 public class Task1 {
     public static void main(String[] arg) {
---
+++ src/com/epam/lessons/lesson1/Task2.java
@@ -0,0 +1,22 @@
+package com.epam.lessons.lesson1;
+
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        // write your
+
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+
+        System.out.println(" *     *  * * * *  *        *           * *  ");
+        System.out.println(" *     *  *        *        *         *     *  ");
+        System.out.println(" * * * *  * * *    *        *        *       * ");
+        System.out.println(" *     *  *        *     *  *     *   *     *" );
+        System.out.print  (" *     *  * * * *  * * * *  * * * *     * *   ");
+        System.out.println(name);
+    }
+}
