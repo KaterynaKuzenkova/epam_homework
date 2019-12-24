52b2a4b5d20332720a2dbd101fe56452a277596b
Kateryna Kuzenkova <katerynakuzenkova@denkuzenkov.local>
2019-12-24 21:18:23 +0200

    first task

---
+++ src/com/epam/lesson1/Lesson1/Task1.java
@@ -0,0 +1,14 @@
+package com.epam.lesson1.Lesson1;
+
+public class Task1 {
+    public static void main(String[] arg) {
+
+
+        System.out.println(" * * * *  * * * *      *        *        * ");
+        System.out.println(" *        *     *     * *       *  *  *  * ");
+        System.out.println(" * *      * * * *   *     *     *    *   * ");
+        System.out.println(" *        *        * * * * *    *        * ");
+        System.out.println(" * * * *  *       *         *   *        * ");
+    }
+}
+
